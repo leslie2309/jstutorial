1c56ca151530172cc60112e65d499b7cc0a24afc
Leslie <santanaleslie18@gmail.com>
2020-05-12 16:21:56 -0700

    commit 1

---
+++ app.js
@@ -0,0 +1,30 @@
+let name ='Leslie' // string
+let age = 30; //number
+let isApproved = true; //boolean
+let firstName = undefined; //undefined
+let selectedColor = null; //null 
+
+
+//person object into one
+let name ='Leslie' 
+let age = 30;
+//this is an object
+let person = { 
+    name: 'Mosh',
+    age: 30
+};
+
+//dot notation
+//change value of object name
+person.name = 'Santana'
+
+//bracket notation
+person ['name'] = 'Janeth' 
+
+//arrays
+let selectedColors = ['red','blue']
+// arrays are dynamic and can change
+selectedColors[2] = 'green'; 
+//displaying an element in an array
+console.log(selectedColors [0]); 
+ 
